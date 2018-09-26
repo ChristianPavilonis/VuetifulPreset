b0bf13fa404b50f1e2c6e616b444d488bbb14b37
Christian Pavilonis <christian@statebuilt.com>
2018-09-26 08:21:00 -0500

    Adds vuex store to app.js

--- src/stubs/app.js
+++ src/stubs/app.js
@@ -3,6 +3,8 @@ import VueRouter   from 'vue-router';
 
 import App  from './Views/App';
 
+import store from './store'
+
 import router from './router';
 
 import './bootstrap';
@@ -13,4 +15,5 @@ const app = new Vue({
     el        : '#app',
     components: { App },
     router,
+    store,
 });
