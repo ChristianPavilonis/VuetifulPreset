b95553f4fdf4e406f2e0f89bee596368e37fcffc
Christian Pavilonis <christian@statebuilt.com>
2018-10-14 14:25:39 -0500

    fix error in webpack.mix.js

--- src/stubs/webpack.mix.js
+++ src/stubs/webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/javascript/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps()
     .webpackConfig({
